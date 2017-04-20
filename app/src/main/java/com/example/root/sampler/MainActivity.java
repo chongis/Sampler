b5193f4a95649ae20fce36089e081b46d6408ac2
Unknown <fponce1@uci.edu>
2017-04-20 15:54:49 -0700

    First Push

---
+++ app/src/main/java/com/example/root/sampler/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.root.sampler;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.LinearLayout;
+import android.widget.Button;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout.LayoutParams;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LinearLayout ll = (LinearLayout)findViewById(R.id.m);
+        ll.setOrientation(LinearLayout.VERTICAL);
+        // add text view
+        TextView tv = new TextView(this);
+        tv.setText("Dynamic Text!");
+        tv.setTextSize(36);
+        tv.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+        ll.addView(tv);
+
+
+    }
+
+    public void test() {
+        LayoutInflater vi = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = vi.inflate(R.layout.activity_main, null);
+
+
+// insert into main view
+        ViewGroup insertPoint = (ViewGroup) findViewById(R.id.m);
+        insertPoint.addView(v, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
+
+    }
+}
---
+++ app/src/main/java/com/example/root/sampler/Makes.java
@@ -0,0 +1,20 @@
+package com.example.root.sampler;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.LinearLayout;
+import android.widget.Button;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout.LayoutParams;
+
+/**
+ * Created by fernando on 4/20/17.
+ */
+
+public class Makes {
+        public Makes(){}
+}
